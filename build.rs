98743879d868dfac47d0a7815db17c52ef6eed37
oakes <zsoakes@gmail.com>
2014-11-30 21:24:16 -0500

    Build neovim as a static lib

--- build.rs
+++ build.rs
@@ -1,8 +1,20 @@
 use std::io::Command;
+use std::os;
 
 fn main() {
-    let cwd = Path::new("neovim");
-    Command::new("make").cwd(&cwd).status().unwrap();
-    let out_dir = "neovim/build/src/nvim";
-    println!("cargo:rustc-flags=-L {} -l nvim:static", out_dir);
+    let curr_dir = os::getenv("CARGO_MANIFEST_DIR").unwrap();
+    let nvim_dir = Path::new(curr_dir).join("neovim");
+    Command::new("make")
+        .cwd(&nvim_dir)
+        .env("SKIP_EXEC", "TRUE")
+        .env("SKIP_UNITTEST", "TRUE")
+        .env("MAKE_LIB", "TRUE")
+        .status()
+        .unwrap();
+
+    let nvim_lib_dir = nvim_dir.join_many(&["build", "src", "nvim"]);
+    let deps_lib_dir = nvim_dir.join_many(&[".deps", "usr", "lib"]);
+    println!("cargo:rustc-flags=-L {} -L {} -l nvim:static -l nvim -l uv -l msgpack -l curses",
+        nvim_lib_dir.as_str().unwrap(),
+        deps_lib_dir.as_str().unwrap());
 }
