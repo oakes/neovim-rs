e899cc0e12c1881515150aeec296612de15f2d08
oakes <zsoakes@gmail.com>
2014-12-01 11:13:28 -0500

    Use new makefile target

--- build.rs
+++ build.rs
@@ -4,16 +4,11 @@ use std::os;
 fn main() {
     let curr_dir = os::getenv("CARGO_MANIFEST_DIR").unwrap();
     let nvim_dir = Path::new(curr_dir).join("neovim");
-    Command::new("make")
-        .cwd(&nvim_dir)
-        .env("MAKE_LIB", "TRUE")
-        .env("SKIP_UNITTEST", "TRUE")
-        .status()
-        .unwrap();
+    Command::new("make").arg("lib").cwd(&nvim_dir).status().unwrap();
 
     let nvim_lib_dir = nvim_dir.join_many(&["build", "src", "nvim"]);
     let deps_lib_dir = nvim_dir.join_many(&[".deps", "usr", "lib"]);
-    println!("cargo:rustc-flags=-L {} -L {} -l nvim:static -l nvim -l uv -l msgpack -l curses",
+    println!("cargo:rustc-flags=-L {} -L {} -l neovim:static -l neovim -l uv -l msgpack -l curses",
         nvim_lib_dir.as_str().unwrap(),
         deps_lib_dir.as_str().unwrap());
 }
