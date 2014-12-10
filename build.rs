ff1f8d4edd99c571be163999a41ee54a1d57e7bf
oakes <zsoakes@gmail.com>
2014-12-10 16:20:39 -0500

    Only run  if neovim hasn't been cloned yet

--- build.rs
+++ build.rs
@@ -1,11 +1,21 @@
 use std::io::Command;
+use std::io::fs::PathExtensions;
 use std::os;
 
 fn main() {
     let curr_dir = Path::new(os::getenv("CARGO_MANIFEST_DIR").unwrap());
-    Command::new("git").arg("submodule").arg("update").arg("--init").cwd(&curr_dir).status().unwrap();
-
     let nvim_dir = curr_dir.join("neovim");
+
+    if !nvim_dir.join("Makefile").exists() {
+        Command::new("git")
+            .arg("submodule")
+            .arg("update")
+            .arg("--init")
+            .cwd(&curr_dir)
+            .status()
+            .unwrap();
+    }
+
     Command::new("make").arg("libnvim").cwd(&nvim_dir).status().unwrap();
 
     let nvim_lib_dir = nvim_dir.join_many(&["build", "src", "nvim"]);
