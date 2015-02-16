e5fb4ef7e74b9d47d472468eea9d28e8a8f6a718
oakes <zsoakes@gmail.com>
2015-02-15 23:29:00 -0500

    Update for latest version of Rust

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::old_io::Command;
 use std::env;
 
 fn main() {
-    let curr_dir = Path::new(env::var("CARGO_MANIFEST_DIR").unwrap().into_string().unwrap());
+    let curr_dir = Path::new(env::var("CARGO_MANIFEST_DIR").unwrap());
     Command::new("git").arg("submodule").arg("update").arg("--init").cwd(&curr_dir).status().unwrap();
 
     let nvim_dir = curr_dir.join("neovim");
