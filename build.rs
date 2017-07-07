00dc7c17a40cb0eb9825023eac5ba3deb01bb7ee
oakes <zsoakes@gmail.com>
2017-07-07 09:36:02 -0400

    Add windows support

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
+#[cfg(not(target_os="windows"))]
 use std::process::Command;
+
 use std::env;
 
 #[cfg(target_os="macos")]
@@ -10,20 +12,30 @@ fn print_lib_dir() {
 fn print_lib_dir() {
 }
 
-fn main() {
+#[cfg(target_os="windows")]
+fn build_lib() {
     let curr_dir = env::current_dir().unwrap();
+    let nvim_dir = curr_dir.join("neovim-windows");
+    println!("cargo:rustc-flags=-L {}",
+        nvim_dir.to_str().unwrap());
+}
+
+#[cfg(not(target_os="windows"))]
+fn build_lib() {
+    let curr_dir = env::current_dir().unwrap();
+    let nvim_dir = curr_dir.join("neovim");
     Command::new("git").arg("submodule").arg("update").arg("--init")
         .current_dir(&curr_dir).status().unwrap();
-
-    let nvim_dir = curr_dir.join("neovim");
     Command::new("make").arg("deps").current_dir(&nvim_dir).status().unwrap();
     Command::new("make").arg("libnvim").current_dir(&nvim_dir).status().unwrap();
-
     let nvim_lib_dir = nvim_dir.join("build").join("lib");
     let deps_lib_dir = nvim_dir.join(".deps").join("usr").join("lib");
     println!("cargo:rustc-flags=-L {} -L {}",
         nvim_lib_dir.to_str().unwrap(),
         deps_lib_dir.to_str().unwrap());
+}
 
+fn main() {
+    build_lib();
     print_lib_dir();
 }
